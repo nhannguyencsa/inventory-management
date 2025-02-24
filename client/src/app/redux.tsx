58cbe2154e73f2bcdcbf8c693d84f6eaf03faefd
nhannguyencsa <nhannguyencsa@gmail.com>
2025-02-24 09:33:50 -0600

    Create products page

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -87,7 +87,7 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>();
+  const storeRef = useRef<AppStore>(makeStore());
   if (!storeRef.current) {
     storeRef.current = makeStore();
     setupListeners(storeRef.current.dispatch);
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -8,6 +8,13 @@ export interface Product {
   stockQuantity: number;
 }
 
+export interface NewProduct { //use Omit<Post, 'productId'> later
+  name: string;
+  price: number;
+  rating?: number;
+  stockQuantity: number;
+}
+
 export interface NewProduct {
   name: string;
   price: number;
@@ -60,13 +67,37 @@ export const api = createApi({
   //api.reducer is created by createApi
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api", // Tên slice này sẽ xuất hiện trong state (state.api)
-  tagTypes: ["DashboardMetrics"], //tagTypes (khai báo danh sách tag) → Khi khởi tạo API.
+  tagTypes: ["DashboardMetrics", "Products"], //tagTypes (khai báo danh sách tag) → Khi khởi tạo API.
   endpoints: (build) => ({
+    //build.query<DataType, ArgumentType>
+    //DataType: Kiểu dữ liệu mà API sẽ trả về.
+    //ArgumentType: Kiểu dữ liệu của tham số truyền vào query
+    //DashboardMetrics: API trả về  kieu DashboardMetrics.
+    //void: API không yêu cầu tham số đầu vào
     getDashboardMetrics: build.query<DashboardMetrics, void>({
       query: () => "/dashboard",
       providesTags: ["DashboardMetrics"], //providesTags (gán tag cho dữ liệu) → Khi fetch dữ liệu.
     }),
+
+    //Product[]:  API trả về một mảng sản phẩm (Product).
+    //string | void:  API có thể nhận tham số kiểu string hoặc không nhận tham số (void).
+    getProducts: build.query<Product[], string | void>({
+      query: (search) => ({
+        url: "/products",
+        params: search ? {search} : {}
+      }),
+      providesTags: ["Products"],
+    }),
+
+    createProduct: build.mutation<Product, NewProduct>({ //use Omit<Post, 'productId'> later
+      query: (newProduct) => ({
+        url: "/products",
+        method: "POST",
+        body: newProduct
+      }),
+      invalidatesTags: ["Products"]
+    })
   }),
 });
 
-export const { useGetDashboardMetricsQuery } = api;
+export const { useGetDashboardMetricsQuery, useGetProductsQuery, useCreateProductMutation } = api;
